0ef220871a6aac0eed2ab256264440d37ddcc7b3
NeilwHart <7ne7il7@gmail.com>
2026-01-22 19:57:10 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,33 @@
-const CACHE_NAME = 'sudoku-cache-v1';
-
-// Using relative paths is safer for GitHub Pages
-const urlsToCache = [
+const CACHE_NAME = 'offline-sudoku-v1';
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './script.js',
   './style.css',
-  './icon-192x192.png',
-  './icon-512x512.png',
-  './manifest.json' // Make sure to include your manifest if you have one!
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-// If you need a base path variable for other logic:
-const GHPATH = '/OfflineSudoku'; 
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        // Add all necessary files to the cache
-        return cache.addAll(urlsToCache); // <-- This is where that line goes
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network-first, fall back to cache for offline
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return the cached file if found
-        if (response) {
-          return response;
-        }
-        // Otherwise, try fetching from the network
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== CACHE_NAME) {
-          console.log('Deleting old cache:', key);
-          return caches.delete(key);
-        }
-      }));
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
